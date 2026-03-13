1f84b135b65440cab6deb716e3b388c5d544e9e6
Rohan Perera <ropereralk@gmail.com>
2026-03-13 13:49:04 +0530

    Added CF

--- SAM/sam-hello-world/hello_world/app.py
+++ SAM/sam-hello-world/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world Edited (Rohan)",
+            "message": "hello world Edited (Rohan) Edited 2",
             # "location": ip.text.replace("\n", "")
         }),
     }
